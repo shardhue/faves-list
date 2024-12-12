0ba91e7168a3cd683d9ee07268f8c1e17293b770
guy12 <190505471+shardhue@users.noreply.github.com>
2024-12-12 23:01:40 +0800

    added update info feature

--- frontend/src/components/CharacterCard.jsx
+++ frontend/src/components/CharacterCard.jsx
@@ -16,9 +16,11 @@ const CharacterCard = ({character}) => {
     };
 
     const [infoShow, setInfoShow] = useState(false);
+    const [updating, setUpdating] = useState(false);
 
     return(
         <>
+
             <br></br>{character.name}
             <br></br>{character.fandom}
             <br></br><img src={character.image} height="300"></img>
@@ -28,23 +30,95 @@ const CharacterCard = ({character}) => {
             {infoShow && (
                 <div className="modal-bg">
                     <div className="modal">
-                        <br></br><input
+
+                        <img src={character.image} height="500"></img>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Name</td>
+                                    <td>
+                                        {!updating && (<>{character.name}</>)}
+                                        {updating && (
+                                            <input
+                                            placeholder='Name'
+                                            name='name'
+                                            value={updatedCharacter.name}
+                                            onChange={(e) => setUpdatedCharacter({ ...updatedCharacter, name: e.target.value })}/>
+                                        )}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Fandom</td>
+                                    <td>
+                                        {!updating && (<>{character.fandom}</>)}
+                                        {updating && (
+                                            <input
+                                            placeholder='Fandom'
+                                            name='fandom'
+                                            value={updatedCharacter.fandom}
+                                            onChange={(e) => setUpdatedCharacter({ ...updatedCharacter, fandom: e.target.value })}/>
+                                        )}
+                                    </td>
+                                </tr>
+                                {updatedCharacter.etc.map((item, i) => {
+                                    return (
+                                        <tr key={i}>
+                                            <td>
+                                                {!updating && (<>{item.label}</>)}
+                                                {updating && (
+                                                    <input
+                                                    placeholder='Label'
+                                                    name='label'
+                                                    value={updatedCharacter.etc[i].label}
+                                                    onChange={(e) => setUpdatedCharacter({ ...updatedCharacter, etc:
+                                                        updatedCharacter.etc.map((item, x) => (x === i ? {...item, label: e.target.value} : item))
+                                                    })}/>
+                                                )}
+                                            </td>
+                                            <td>
+                                                {!updating && (<>{item.field}</>)}
+                                                {updating && (
+                                                    <>
+                                                        <input
+                                                        placeholder='Field'
+                                                        name='field'
+                                                        value={updatedCharacter.etc[i].field}
+                                                        onChange={(e) => setUpdatedCharacter({ ...updatedCharacter, etc:
+                                                            updatedCharacter.etc.map((item, x) => (x === i ? {...item, field: e.target.value} : item))
+                                                        })}/>
+                                                        <button onClick={() => setUpdatedCharacter({...updatedCharacter, etc:
+                                                            [...updatedCharacter.etc.filter((item, y) => y !== i)]
+                                                        })}>-</button>
+                                                    </>
+                                                )}
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+
+                        {/* <input
                         placeholder='Character Name'
                         name='name'
                         value={updatedCharacter.name}
-                        onChange={(e) => setUpdatedCharacter({ ...updatedCharacter, name: e.target.value })}/>
-                        <br></br><input
-                        placeholder='Fandom'
-                        name='fandom'
-                        value={updatedCharacter.fandom}
-                        onChange={(e) => setUpdatedCharacter({ ...updatedCharacter, fandom: e.target.value })}/>
-                        <br></br><input
-                        placeholder='Image URL'
-                        name='image'
-                        value={updatedCharacter.image}
-                        onChange={(e) => setUpdatedCharacter({ ...updatedCharacter, image: e.target.value })}/>
-                        <br></br><button onClick={() => handleUpdatedCharacter(character._id, updatedCharacter)}>Update</button>
-                        <button onClick={() => setInfoShow(false)}>Close</button>
+                        onChange={(e) => setUpdatedCharacter({ ...updatedCharacter, name: e.target.value })}/> */}
+
+                        {!updating && (
+                            <>
+                                <button onClick={() => setUpdating(true)}>Update</button>
+                                <button onClick={() => setInfoShow(false)}>Close</button>
+                            </>
+                        )}
+                        {updating && (
+                            <>
+                                <button onClick={() => setUpdatedCharacter({...updatedCharacter, etc:
+                                    [...updatedCharacter.etc, {label: "", field: ""}]
+                                })}>Add Field</button><br></br>
+                                <button onClick={() => {handleUpdatedCharacter(character._id, updatedCharacter), setUpdating(false)}}>Save</button>
+                                <button onClick={() => {setUpdatedCharacter(character), setUpdating(false)}}>Cancel</button>
+                            </>
+                        )}
                     </div>
                 </div>
             )}
